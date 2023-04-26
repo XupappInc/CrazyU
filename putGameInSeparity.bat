c5a6b3137dadafd28fcbfff48fc859faedcb1eed
Laura Gomez <lgomez25@ucm.es>
2023-04-26 15:27:54 +0200

    ajustes con los .bat

---
+++ putGameInSeparity.bat
@@ -0,0 +1,21 @@
+@echo off
+
+mkdir Separity\Exes\Assets
+
+set EXES=Separity\Exes
+
+cd Separity
+
+call buildSeparity.bat
+
+cd ..
+
+call buildGame.bat
+
+XCOPY /y /s lib\x64\Debug\SeparityGame_d.dll %EXES%
+XCOPY /y /s lib\x64\Release\SeparityGame.dll %EXES%
+XCOPY /y /s Separity\lib\x64\Debug\SeparityExports_d.dll %EXES%
+XCOPY /y /s Separity\lib\x64\Release\SeparityExports.dll %EXES%
+XCOPY /y /s Assets\ %EXES%\Assets
+
+
