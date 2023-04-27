eb093c372c51be4a87acbb1bc0f61f9205f6e602
Laura Gomez <lgomez25@ucm.es>
2023-04-27 02:01:22 +0200

    Movidas cosas del juego al motor

--- Src/GameStart.cpp
+++ Src/GameStart.cpp
@@ -1,12 +1,3 @@
-// Separity.cpp : Este archivo contiene la función "main". La ejecución del
-// programa comienza y termina ahí.
-// #include <Windows.h>
-//
-// #ifdef _DEBUG
-// HMODULE hinstLib = LoadLibrary(TEXT("Separity_Game_d"));
-// #else
-// HMODULE hinstLib = LoadLibrary(TEXT("Separity_Game"));
-// #endif  // DEBUG
 
 #include "EntityComponent\Entity.h"
 
@@ -25,6 +16,7 @@
 #include "UIEngine\Button.h"
 #include "UIEngine\Panel.h"
 #include "UIEngine\Text.h"
+#include "VehicleMovement.h"
 
 // Managers
 #include "EntityComponent\EntityManager.h"
@@ -42,7 +34,7 @@
 #include "GameStart.h"
 #include "SeparityUtils\Random.h"
 #include "SeparityUtils\checkML.h"
-//#include "SeparityUtils/VirtualTimer.h"
+#include "SeparityUtils\VirtualTimer.h"
 #include <Windows.h>
 #include <iostream>
 #include <utility>
@@ -51,17 +43,19 @@ const uint32_t FRAMERATE = 60;
 const uint32_t FRAMETIME = 1000 / FRAMERATE;
 using namespace Separity;
 int CrazyU::GameStart::initJuego() {
+
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	GetComponentWrapper::createAllManagers();
 
 	GetComponentWrapper::registerInLua();
-	RenderManager* rm = RenderManager::getInstance();
+	
+	//RenderManager* rm = RenderManager::getInstance();
 	ManagerManager* mm = Separity::ManagerManager::getInstance();
 	InputManager* inputManager = Separity::InputManager::getInstance();
 	EntityManager* entityManager = Separity::EntityManager::getInstance();
 	AudioManager* audioManager = Separity::AudioManager::getInstance();
 	;
-
+	
 	SceneManager* sceneMenager = Separity::SceneManager::getInstance();
 	sceneMenager->loadScene("menuScene.lua");
 
@@ -74,12 +68,6 @@ int CrazyU::GameStart::initJuego() {
 	Entity* sinbad = entityManager->addEntity(_grp_GENERAL);
 	sinbad->getComponent<Transform>()->translate({-15, 60, 12});
 	sinbad->addComponent<MeshRenderer>()->setMesh("Sinbad.mesh");
-	// sinbad->addComponent<Text>(
-	//     "texto", "fuentePrueba",
-	//                            0, 0, 10,
-	//                            10, "hola",
-	//                            Spyutils::Vector3(1,1,1));
-	// luaManager->loadScript("prueba", sinbad);
 
 	Entity* guile = entityManager->addEntity(_grp_GENERAL);
 	guile->getComponent<Transform>()->translate({0, 10, 12});
@@ -89,7 +77,6 @@ int CrazyU::GameStart::initJuego() {
 	Entity* sinbad3 = entityManager->addEntity(_grp_GENERAL);
 	sinbad->addChild(sinbad3);
 	sinbad3->getComponent<Transform>()->translate({0, 5, 0});
-	// sinbad3->getComponent<Transform>()->roll(90);
 	sinbad3->addComponent<MeshRenderer>()->setMesh("Sinbad.mesh");
 
 	colliderParams params;
@@ -103,26 +90,13 @@ int CrazyU::GameStart::initJuego() {
 	sinbad->addComponent<RigidBody>(DYNAMIC, 10);
 	auto animSinbad = sinbad->addComponent<Animator>();
 
-	/*Entity* luz = entityManager->addEntity(_grp_GENERAL);
-	auto luzGlobal = luz->addComponent<Light>(DIRECTIONAL_LIGHT);
-	luzGlobal->setDiffuse({0.7, 0.7, 0.7});
-	auto luzTr = luz->getComponent<Transform>();
-	luzTr->translate({0, 100, 0});
-
-	Entity* luzAux = entityManager->addEntity(_grp_GENERAL);
-	auto luzAux1 = luzAux->addComponent<Light>(DIRECTIONAL_LIGHT);
-	auto luzTr2 = luzAux->getComponent<Transform>();
-	luzTr2->translate({0, 100, 0});
-	luzAux1->setDirection({0, 0, -1});
-	luzAux1->setDiffuse({0.5, 0, 0.5});*/
-
 	Entity* button = entityManager->addEntity(_grp_GENERAL);
 	Text* txt =
 	    button->addComponent<Text>("TextoPrueba", "fuentePrueba", 100, 100, 200,
 	                               50, "Holi", Spyutils::Vector3(1, 1, 1));
 	
-	/*Camera* cam_cam = RenderManager::getInstance()->getCamera();
-	Entity* camera = cam_cam->getEntity();
+	Camera* cam_cam = RenderManager::getInstance()->getCamera();
+	/*Entity* camera = cam_cam->getEntity();
 	Transform* cam_tr = camera->getComponent<Transform>();*/
 
 	// BUS
@@ -131,7 +105,8 @@ int CrazyU::GameStart::initJuego() {
 	cocheTr->translate({0, 10, 0});
 	cocheTr->setScale(1);
 	coche->addComponent<MeshRenderer>()->setMesh("Bus1.mesh");
-	
+	/*VehicleMovement* coche_vehiculo =
+	    coche->addComponent<VehicleMovement>(cam_tr);*/
 
 	colliderParams paramsCoche;
 	paramsCoche.colShape = CUBE;
@@ -145,20 +120,20 @@ int CrazyU::GameStart::initJuego() {
 
 	//coche->addChild(camera);
 	Spyutils::Vector3 posCoche = cocheTr->getPosition();
-	//cam_tr->setPosition(posCoche.x, posCoche.y + 3, posCoche.z + 7.5);
-	//cam_tr->pitch(20);
+	/*cam_tr->setPosition(posCoche.x, posCoche.y + 3, posCoche.z + 7.5);
+	cam_tr->pitch(20);*/
 
 	//Spyutils::VirtualTimer* timer = new Spyutils::VirtualTimer();
 	uint32_t deltaTime = 0;
 
 	bool rot = true;
-
 	mm->initComponents();
 
+
 	while(!mm->quit() && !InputManager::getInstance()->closeWindowEvent()) {
-		//cam_cam = RenderManager::getInstance()->getCamera();
-		//camera = cam_cam->getEntity();
-		//cam_tr = camera->getComponent<Transform>();
+		cam_cam = RenderManager::getInstance()->getCamera();
+		/*camera = cam_cam->getEntity();
+		cam_tr = camera->getComponent<Transform>();*/
 
 		//timer->reset();
 
@@ -170,8 +145,8 @@ int CrazyU::GameStart::initJuego() {
 			if(inputManager->isKeyUp('s')) {
 				rot = true;
 				//auto quate = cam_tr->getRotationQ();
-				//quate.rotateGlobal(180, {0, 1, 0});
-				//cam_tr->setRotationQ(quate.w, quate.x, quate.y, quate.z);
+				/*quate.rotateGlobal(180, {0, 1, 0});
+				cam_tr->setRotationQ(quate.w, quate.x, quate.y, quate.z);*/
 			}
 			
 			if(inputManager->isKeyDown('c')) {
@@ -181,12 +156,6 @@ int CrazyU::GameStart::initJuego() {
 				auto trsi = sinbad->getComponent<Transform>();
 				trsi->setScale(20);
 			}
-			/*if(inputManager->isKeyDown('l')) {
-			    animSinbad->playAnim("Dance", false);
-			}
-			if(inputManager->isKeyDown('k')) {
-			    animSinbad->playAnim("Dance");
-			}*/
 		}
 
 		mm->update(deltaTime);
