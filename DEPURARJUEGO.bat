bd1700639bc390f2cc165ee7287ff0260039c1cc
Laura <lgomez25@ucm.es>
2023-04-29 19:56:58 +0200

    Arreglado bat de depuración

--- DEPURARJUEGO.bat
+++ DEPURARJUEGO.bat
@@ -1,11 +1,10 @@
 @echo off
-cls
+
 mkdir Separity\Exes\Assets
 
 set EXES=Separity\Exes
 
 cd Separity
-
 call buildSeparity.bat
 
 cd ..
@@ -16,5 +15,6 @@ XCOPY /y /s lib\Debug\SeparityGame_d.dll %EXES%
 XCOPY /y /s lib\Release\SeparityGame.dll %EXES%
 XCOPY /y /s Separity\lib\x64\Debug\SeparityExports_d.dll %EXES%
 XCOPY /y /s Separity\lib\x64\Release\SeparityExports.dll %EXES%
+XCOPY /y /s Exe\Assets\ %EXES%\Assets
 
-XCOPY /y /s Exe/Assets %EXES%\Assets
+pause
