01d74b1c7781d787dc163d1f127426114379aaec
Laura Gomez <lgomez25@ucm.es>
2023-05-01 23:41:14 +0200

    pathfollow

--- Exe/Assets/Scenes/gameScene.lua
+++ Exe/Assets/Scenes/gameScene.lua
@@ -14258,5 +14258,16 @@ Entities = {
 			dim = {27.082321166992188, 27.144329071044922, 42.51074981689453},
 			type = 2
 		}
+	},
+	JoseAntonio = {
+		meshRenderer={
+			meshName = "guille.mesh"
+		},
+		pathFollow={
+			nWaypoints = 3,
+			waypoint0 = {4,3,5},
+			waypoint1 = {14,3,5},
+			waypoint2 = {4,13,5},
+		}
 	}
 }
