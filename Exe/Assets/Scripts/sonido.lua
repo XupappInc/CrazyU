01a86f3b81bccc5efaa70e1b0e5be3a303b7e9e2
damart24 <82331644+damart24@users.noreply.github.com>
2023-05-02 19:19:02 +0200

    creado script para sonidos

--- Exe/Assets/Scripts/sonido.lua
+++ Exe/Assets/Scripts/sonido.lua
@@ -1,66 +1,66 @@
 --Definicion de la clase en Lua
-musica = {}
+sonido = {}
 
-musica.__index = musica
+sonido.__index = sonido
 
 --Constructor de la clase en Lua
-function musica:new()
+function sonido:new()
 	local obj={}
-	setmetatable(obj, musica)
+	setmetatable(obj, sonido)
 	return obj
 end
-local notPlaying=true;
+
 --Metodo Awake, llamado al comienzo de la ejecucion
-function musica:awake()
+function sonido:awake()
 
 end
 
 --Metodo Start, llamado tras Awake
-function musica:start()
-	print("Estoy entrando en el start de la musica");
-	audioName = musica.entity:getAudio():getAudioName();
-	AudioManager:play(audioName, 10000, 10000);
+function sonido:start()
+	print("Estoy entrando en el start del sonido");
+	audioName = sonido.entity:getAudio():getAudioName();
+	AudioManager:play(audioName, 1, 100);
 end
 
 --Metodo Update, llamado en cada frame
-function musica:update()
+function sonido:update()
 
 end
 
 --Metodo OnCollisionEnter, llamado al comenzar una colision
-function musica:onCollisionEnter()
+function sonido:onCollisionEnter()
 
 end
 
 --Metodo OnCollisionStay, llamado mientras se mantenga una colision
-function musica:onCollisionStay()
+function sonido:onCollisionStay()
 
 end
 
 --Metodo OnCollisionExit, llamado al salir de una colision
-function musica:onCollisionExit()
+function sonido:onCollisionExit()
 
 end
 
 --Metodo OnButtonClick, llamado al pulsar el boton de esta entidad
-function musica:onButtonClick()
+function sonido:onButtonClick()
 
 end
 
 --Metodo OnButtonReleased, llamado al soltar el boton de esta entidad
-function musica:onButtonReleased()
+function sonido:onButtonReleased()
 
 end
 
 --Metodo OnButtonHover, llamado al posar el raton sobre el boton de esta entidad
-function musica:onButtonHover()
+function sonido:onButtonHover()
 
 end
 
 --Metodo OnButtonUnhover, llamado al dejar de posar el raton sobre el boton de esta entidad
-function musica:onButtonUnhover()
+function sonido:onButtonUnhover()
 
 end
 
 --Variable global de la clase (para Luabridge)
-musicaLua = musica:new()
+sonidoLua = sonido:new()
