0b10ca21d25e7ab6aed115b0e5903a1775fd9d64
Sergio Baña <sebana@ucm.es>
2023-05-03 22:07:35 +0200

    Update buildGame.bat

--- buildGame.bat
+++ buildGame.bat
@@ -5,5 +5,8 @@ cls
 msbuild "CrazyU.sln" /p:Configuration=Debug /p:Platform=x64
 msbuild "CrazyU.sln" /p:Configuration=Release /p:Platform=x64
 
+XCOPY /y /s lib\Debug\SeparityGame_d.dll Exe
+XCOPY /y /s lib\Release\SeparityGame.dll Exe
+
 cd Exe
 call Separity.exe
